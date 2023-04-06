ee5c077223548a6e13f7a2461392f7de49300c98
xlcxy2 <130036323+xlcxy2@users.noreply.github.com>
2023-04-06 16:31:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -100,7 +100,6 @@ const USER_CONFIG = {
   // 每日一言的内容类型
   // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
   LITERARY_PREFERENCE: '',
-  
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
@@ -108,7 +107,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '超人',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
       id: 'oatre6lpoJq9jpsVvZOfsies1mnk',
@@ -213,6 +212,46 @@ const USER_CONFIG = {
       },
     },
     {
+      // 想要发送的人的名字
+      name: '怪兽',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      // 使用其他通道时，请严格按照各个通道的教程进行填写
+      id: 'oatre6lpoJq9jpsVvZOfsies1mnk',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
+      useTemplateId: 'VqBzCral3Q6RWdfKv7HsTnXD_gsuKDspogbuktJtA9Q',
+      // 所在省份或城市，也可以不填
+      province: '阿克苏',
+      // 所在城市或县区
+      city: '温宿',
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '08-29',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: 'https://wangxinleo.cn',
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '生日', name: '怪兽', year: '2001', date: '08-29',
+        },
+       
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '超人', year: '2001', date: '12-22',
+        },
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2020-04-03' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ], 
+      },
+    },
+    {
       name: '老婆1',
       id: '',
       useTemplateId: '',
@@ -225,32 +264,7 @@ const USER_CONFIG = {
       customizedDateList: [],
       courseSchedule: null
     },
-    {
-      name: '老婆2',
-      id: '',
-      useTemplateId: '',
-      province: '',
-      city: '',
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: 'https://wangxinleo.cn',
-      festivals: [],
-      customizedDateList: [],
-      courseSchedule: null
-    },
-    {
-      name: '老婆3',
-      id: '',
-      useTemplateId: '',
-      province: '',
-      city: '',
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: 'https://wangxinleo.cn',
-      festivals: [],
-      customizedDateList: [],
-      courseSchedule: null
-    }
+ 
     // 你可以不断按格式往下增加
     // ...
   ],
