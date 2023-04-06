d4498e11d5d82940729063d1eeb9360be5719db4
xlcxy2 <130036323+xlcxy2@users.noreply.github.com>
2023-04-06 16:05:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -114,7 +114,7 @@ const USER_CONFIG = {
       id: 'oatre6lpoJq9jpsVvZOfsies1mnk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'Xt5l0MKv-xXPu2zwCxRrm0ZLntKvH6msbeBDUjnwrzg',
+      useTemplateId: 'UXwNeeZciRB7_ZhxARxy18y4Kp-QE-Y8jMkMie5lleQ',
       // 所在省份或城市，也可以不填
       province: '阿克苏',
       // 所在城市或县区
